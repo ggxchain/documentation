0b628b57bff91b4ac0ce2e21090b0ad1603045aa
uchu <pavel.burylichev@gmail.com>
2023-07-26 15:27:05 +0400

    Various improvements on styling and a theme plugin

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # GGX Documentation
 
-Documentation consists of `.md` files, but it can be built into a static website.
+The documentation consists of `.md` files, but it can be built into a static website.
 
 ## How to run the project
 
---
+++ _plugins/ggx-theme/assets/theme.js
@@ -0,0 +1,41 @@
+require(['gitbook'], function (gitbook, $) {
+    setDefaultTheme(gitbook.storage.get('fontState'));
+
+    gitbook.events.bind('start', function (e, config) {
+        gitbook.toolbar.createButton({
+            icon: 'fa fa-moon-o',
+            label: 'Night Mode',
+            position: 'right',
+            // Switch the themes
+            onClick: function () {
+                let fontState = gitbook.storage.get('fontState');
+                const book = document.querySelector('div.book');
+                if (book.classList.contains('color-theme-2')) {
+                    book.classList.remove('color-theme-2');
+                    book.classList.add('color-theme-0');
+                    fontState.theme = 0;
+                } else {
+                    book.classList.remove('color-theme-0');
+                    book.classList.remove('color-theme-1');
+                    book.classList.add('color-theme-2');
+                    fontState.theme = 2;
+                }
+                gitbook.storage.set('fontState', fontState);
+            }
+        });
+    });
+
+    function setDefaultTheme(fontState) {
+        if (!fontState && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            fontState = {
+                family: 1,
+                size: 2,
+                theme: 2, // Dark theme
+            }
+            gitbook.storage.set('fontState', fontState);
+        }
+    }
+
+});
+
+
